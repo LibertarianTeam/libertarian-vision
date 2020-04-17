6a360dd26a2ce532ee7de51a6626542389bf86b2
paulloclara <paulloclara@gmail.com>
2020-04-17 11:44:22 -0300

    :rocket: Add initial button component

---
+++ src/components/Button.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: "Button",
+  props: ["type", "to"],
+  methods: {
+    emitClick(evt) {
+      this.$emit("click", evt);
+    }
+  }
+};
+</script>
+
+<template lang="html">
+  <router-link
+    v-if="type === 'link' && to.name"
+    :to="to"
+    class="button"
+    @click="emitClick($event)"
+  >
+    <slot name="default"></slot>
+  </router-link>
+
+  <a
+    v-else-if="type === 'link'"
+    class="button"
+    :href="to"
+    target="_blank"
+    @click="emitClick($event)"
+  >
+    <slot name="default"></slot>
+  </a>
+
+  <button v-else :type="type" class="button" @click="emitClick($event)">
+    <slot name="default"></slot>
+  </button>
+</template>
+
+<style lang="css" scoped>
+.button {
+  display: inline-block;
+
+  padding: 4px 8px;
+
+  border: 1px solid #000;
+  border-radius: 2px;
+
+  color: var(--text-primary);
+  font-size: 14px;
+  text-align: center;
+  font-weight: medium;
+  text-decoration: none;
+
+  cursor: pointer;
+  transition: filter 0.2s;
+
+  background-color: var(--primary);
+}
+
+.button:hover {
+  filter: brightness(90%);
+}
+</style>
