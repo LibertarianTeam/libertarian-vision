b6ebda161359df45c4d712a807cd7a4af75d1d17
paulloclara <paulloclara@gmail.com>
2020-04-17 21:18:53 -0300

    :rocket: Config static store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import staticStore from "@/store/static";
 import standardStore from "@/store/standard";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  ...standardStore
+  ...standardStore,
+  modules: {
+    static: staticStore
+  }
 });
