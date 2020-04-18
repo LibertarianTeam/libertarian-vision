2e8fd97187597ef577687b9cf50e761cef6dc200
paulloclara <paulloclara@gmail.com>
2020-04-17 21:20:13 -0300

    :rocket: Add nav items

--- src/store/static.js
+++ src/store/static.js
@@ -7,6 +7,109 @@ export default {
       youtube: "https://youtube.com/ancapsu",
       bitchute: "https://bitchute.com/ancapsu",
       facebook: "https://facebook.com/pageancapsu"
+    },
+    navItems: {
+      video: {
+        text: "Vídeos",
+        to: { name: "Category", params: { category: "video", filter: "all" } },
+        items: [
+          {
+            text: "Teoria libertária",
+            to: {
+              name: "Category",
+              params: { category: "video", filter: "theory" }
+            }
+          },
+          {
+            text: "Visão libertária",
+            to: {
+              name: "Category",
+              params: { category: "video", filter: "news" }
+            }
+          },
+          {
+            text: "Cômicos",
+            to: {
+              name: "Category",
+              params: { category: "video", filter: "comic" }
+            }
+          },
+          {
+            text: "Procurar",
+            to: {
+              name: "Category",
+              params: { category: "video", filter: "search" }
+            }
+          }
+        ]
+      },
+      matter: {
+        text: "Metérias",
+        to: { name: "Category", params: { category: "matter", filter: "all" } },
+        items: [
+          {
+            text: "Artigos",
+            to: {
+              name: "Category",
+              params: { category: "matter", filter: "article" }
+            }
+          },
+          {
+            text: "Tapas",
+            to: {
+              name: "Category",
+              params: { category: "matter", filter: "shortnote" }
+            }
+          },
+          {
+            text: "Crônicas",
+            to: {
+              name: "Category",
+              params: { category: "matter", filter: "chronicle" }
+            }
+          },
+          {
+            text: "Procurar",
+            to: {
+              name: "Category",
+              params: { category: "matter", filter: "search" }
+            }
+          }
+        ]
+      },
+      schedule: {
+        text: "Pautas",
+        to: {
+          name: "Category",
+          params: { category: "schedule", filter: "all" }
+        },
+        items: [
+          {
+            text: "Últimas pautas",
+            to: {
+              name: "Category",
+              params: { category: "schedule", filter: "news" }
+            }
+          },
+          {
+            text: "Procurar",
+            to: {
+              name: "Category",
+              params: { category: "schedule", filter: "search" }
+            }
+          }
+        ]
+      },
+      other: {
+        text: "Sobre",
+        to: { name: "About" },
+        items: [
+          {
+            text: "Ajuda",
+            to: { name: "Help" }
+          }
+        ]
+      }
     }
   }
 };
