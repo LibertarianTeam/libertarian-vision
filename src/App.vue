2e4e747dc4b6043a273d109e3e75489311b5acde
paulloclara <paulloclara@gmail.com>
2020-04-21 15:00:25 -0300

    :rocket: Config initial light theme

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,15 @@ export default {
 
 :root {
   --primary: #fc0;
+  --secondary: #03a9f4;
+  --error: #f44336;
+  --warning: #ffeb3b;
+  --success: #4caf50;
+  --info: #ffc107;
+  --accent: #607d8b;
   --text-primary: #000;
+  --text-secondary: #fff;
+  --text-tertiary: #fc0;
 }
 
 #app {
