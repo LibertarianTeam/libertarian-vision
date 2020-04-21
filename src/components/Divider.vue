7aa3dbd03931d85a1c3042d27a5d8531011f3580
paulloclara <paulloclara@gmail.com>
2020-04-21 15:01:23 -0300

    :rocket: Add initial divider component

---
+++ src/components/Divider.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "Divider"
+};
+</script>
+
+<template lang="html">
+  <div class="divider"></div>
+</template>
+
+<style lang="css" scoped>
+.divider {
+  width: 100%;
+  height: 2px;
+
+  margin: 8px 0 16px;
+
+  background-color: #000;
+}
+</style>
