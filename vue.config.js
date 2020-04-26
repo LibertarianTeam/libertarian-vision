bf855f9eec8383dc4e5f277c75c31f532bfb301f
paulloclara <paulloclara@gmail.com>
2020-04-26 08:11:24 -0300

    :rocket: Config pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  lintOnSave: false,
   outputDir: "production/dist",
-  lintOnSave: false
+  pwa: {
+    name: "AncapSU",
+    display: "standalone",
+    themeColor: "#fc0",
+    msTileColor: "#fc0",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#fc0",
+    manifestOptions: {
+      start_url: ".",
+      short_name: "AncapSU",
+      description: "Imposto é roubo e sonegar é legitima defesa.",
+      background_color: "#000"
+    }
+  }
 };
