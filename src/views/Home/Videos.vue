597e2cab91dbce9c64e96dcbba50e65cb2dcf822
paulloclara <paulloclara@gmail.com>
2020-04-27 09:53:55 -0300

    :fire: Enhanced sample data

--- src/views/Home/Videos.vue
+++ src/views/Home/Videos.vue
@@ -3,7 +3,7 @@ import Title from "@/views/Home/Title";
 import Carousel from "@/components/Carousel";
 
 export default {
-  name: "HomeVideos",
+  name: "HomeVídeos",
   components: {
     "m-title": Title,
     "c-carousel": Carousel
@@ -12,46 +12,54 @@ export default {
     return {
       items: [
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/typewriter.png",
-          tag: "Video",
-          externalImg: false
+          title:
+            "Presidente das Filipinas manda matar quem descumprir quarentena.",
+          img: "https://i.ibb.co/n0PJpgL/image.png",
+          tag: "Vídeo",
+          externalImg: true
         },
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/mic.png",
-          tag: "Video",
-          externalImg: false
+          title: "Estão faltando mortos nessa conta.",
+          img: "https://i.ibb.co/WFhvVym/image.png",
+          tag: "Vídeo",
+          externalImg: true
         },
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/news.png",
-          tag: "Video",
-          externalImg: false
+          title: "Chegou a hora de o Brasil ter um Banco Central independente?",
+          img: "https://i.ibb.co/DLdmLWW/image.png",
+          tag: "Vídeo",
+          externalImg: true
         },
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/typewriter.png",
-          tag: "Video",
-          externalImg: false
+          title: "Venezuela está afundando seus próprios navios.",
+          img: "https://i.ibb.co/LpHFy7n/image.png",
+          tag: "Vídeo",
+          externalImg: true
         },
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/mic.png",
-          tag: "Video",
-          externalImg: false
+          title:
+            "Presidente das Filipinas manda matar quem descumprir quarentena.",
+          img: "https://i.ibb.co/n0PJpgL/image.png",
+          tag: "Vídeo",
+          externalImg: true
         },
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/news.png",
-          tag: "Video",
-          externalImg: false
+          title: "Estão faltando mortos nessa conta.",
+          img: "https://i.ibb.co/WFhvVym/image.png",
+          tag: "Vídeo",
+          externalImg: true
         },
         {
-          title: "Venezuela está afundando seus próprios navios",
-          img: "imgs/typewriter.png",
-          tag: "Video",
-          externalImg: false
+          title: "Chegou a hora de o Brasil ter um Banco Central independente?",
+          img: "https://i.ibb.co/DLdmLWW/image.png",
+          tag: "Vídeo",
+          externalImg: true
+        },
+        {
+          title: "Venezuela está afundando seus próprios navios.",
+          img: "https://i.ibb.co/LpHFy7n/image.png",
+          tag: "Vídeo",
+          externalImg: true
         }
       ]
     };
@@ -62,14 +70,13 @@ export default {
 <template lang="html">
   <div class="home-videos">
     <m-title>Últimos Vídeos do Canal</m-title>
-    <c-carousel :items="items"></c-carousel>
+    <c-carousel :items="items" :centerBG="false"></c-carousel>
   </div>
 </template>
 
 <style lang="css" scoped>
 .carousel {
   width: 100%;
-
   margin: 0 auto;
 }
 </style>
