cf737be0b0da7ccded3325a74f406529fa5c8dc4
paulloclara <paulloclara@gmail.com>
2020-05-01 16:16:04 -0300

    :rocket: Add double divider option

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,21 +1,35 @@
 <script>
+import { buildClass } from "@/utils";
 import Divider from "@/components/Divider";
 
 export default {
   name: "Title",
   components: {
     "gc-divider": Divider
+  },
+  props: {
+    double: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classTitle() {
+      return buildClass("gc-title", ["double"], this.$props);
+    }
   }
 };
 </script>
 
 <template lang="html">
-  <div class="gc-title">
+  <div :class="classTitle">
     <gc-divider></gc-divider>
 
     <h3>
       <slot name="default"></slot>
     </h3>
+
+    <gc-divider v-if="double"></gc-divider>
   </div>
 </template>
 
@@ -23,5 +37,9 @@ export default {
 .gc-title {
   margin-bottom: 20px;
   font: bold 24px caption;
+}
+
+.double .gc-divider {
+  margin: 8px 0;
 }
 </style>
