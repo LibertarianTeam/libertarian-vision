81d17e74ec7104ac4894b9b41f9475ae5d296f3b
paulloclara <paulloclara@gmail.com>
2020-05-19 14:39:22 -0300

    :rocket: Add initial loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <div class="c-loading"></div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style lang="css" scoped>
+.c-loading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 20;
+  position: fixed;
+
+  background-color: var(--bg-primary);
+}
+
+.c-loading::after {
+  content: '';
+
+  border: 60px solid #666;
+  border-top-color: var(--primary);
+  border-bottom-color: var(--primary);
+  border-radius: 50%;
+
+  animation: spin 1.4s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,29 +1,46 @@
 <script>
-import { mapMutations } from 'vuex'
 import AppBar from '@/views/AppBar'
 import FooterBar from '@/views/FooterBar'
+import Loading from '@/components/Loading'
 
 export default {
   name: 'DefaultLayout',
   components: {
     'v-app-bar': AppBar,
+    'c-loading': Loading,
     'v-footer-bar': FooterBar
   },
+  data() {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    appClass() {
+      return `light-theme${this.$data.show ? ' show' : ''}`
+    }
+  },
   mounted() {
-    this.updateWindowSize()
-    window.addEventListener('resize', this.updateWindowSize)
+    this.$store.commit('updateWindowSize')
+
+    window.addEventListener('resize', () => {
+      this.$store.commit('updateWindowSize')
+    })
+
+    this.$data.show = true
   },
   destroyed() {
-    window.removeEventListener('resize', this.updateWindowSize)
-  },
-  methods: {
-    ...mapMutations(['updateWindowSize'])
+    window.removeEventListener('resize', () => {
+      this.$store.commit('updateWindowSize')
+    })
   }
 }
 </script>
 
 <template lang="html">
-  <div id="app" class="light-theme">
+  <div id="app" :class="appClass">
+    <c-loading v-if="!$data.show"></c-loading>
+
     <v-app-bar></v-app-bar>
     <nuxt></nuxt>
     <v-footer-bar></v-footer-bar>
@@ -40,6 +57,15 @@ export default {
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
+}
+
+#app > *:not(.c-loading) {
+  opacity: 0;
+  transition: opacity 1.4s;
+}
+
+#app.show > * {
+  opacity: 1;
 }
 
 .light-theme {
@@ -65,5 +91,7 @@ export default {
   --bx-primary: #0002;
   --bx-secondary: #0004;
   --bx-tertiary: #0008;
+
+  --bg-primary: #fff;
 }
 </style>
