7dfedc37bddbe20d89cfab869d1e6d260a924165
paulloclara <paulloclara@gmail.com>
2020-06-14 10:04:18 -0300

    :rocket: Add reading section

---
+++ src/pages/_read/_id.vue
@@ -0,0 +1,20 @@
+<script>
+import GenerateMetaInfo from '~/plugins/vue-meta'
+
+export default {
+  name: 'Read',
+  head: GenerateMetaInfo({
+    page: {
+      title: 'Leitura'
+    }
+  })
+}
+</script>
+
+<template lang="html">
+  <main id="p-read">
+    {{ $route.params }}
+  </main>
+</template>
+
+<style lang="css" scoped></style>
