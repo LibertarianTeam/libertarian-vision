815c05f71a41cbd9da15da3b25bddcc2a31f82b1
PaulloClara <paulloclara@gmail.com>
2023-04-16 19:18:17 -0300

    feat: adiciona assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: { head: { title: "Visão Libertária", htmlAttrs: { lang: "pt-br" } } },
   css: ["~/assets/styles/main.css"],
   postcss: {
     plugins: {
